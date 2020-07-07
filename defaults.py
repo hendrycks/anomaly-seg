10d3f971908404f6b342e5c2156ed8fc07e36a37
Steven Basart <xksteven@users.noreply.github.com>
2020-07-07 19:53:07 -0400

    Changes default classes to reflect StreetHazards
    Number of classes is 17 for StreetHazards

--- defaults.py
+++ defaults.py
@@ -14,7 +14,7 @@ _C.DATASET = CN()
 _C.DATASET.root_dataset = "./data/"
 _C.DATASET.list_train = "./data/training.odgt"
 _C.DATASET.list_val = "./data/validation.odgt"
-_C.DATASET.num_class = 150
+_C.DATASET.num_class = 17
 # multiscale train/test, size of short edge (int or tuple)
 _C.DATASET.imgSizes = (300, 375, 450, 525, 600)
 # maximum input image size of long edge
